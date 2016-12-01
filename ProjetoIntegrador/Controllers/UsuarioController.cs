bb5d7a49e9bbfd67a34d71059b4dfb8f02f84628
ZiulLopes <lopesluiz_@hotmail.com>
2016-12-01 17:19:23 -0200

    JCROP
    Jcrop teve um bom desenpenho. Vou implementar.

--- ProjetoIntegrador/Controllers/UsuarioController.cs
+++ ProjetoIntegrador/Controllers/UsuarioController.cs
@@ -56,5 +56,44 @@ namespace ProjetoIntegrador.Controllers
             }
             return View(user);
         }
+		
+		//
+        // POST: /Usuario/CropImage
+		[HttpPost]
+        public virtual ActionResult CropImage(
+            string imagePath,
+            int? cropPointX,
+            int? cropPointY,
+            int? imageCropWidth,
+            int? imageCropHeight)
+        {
+            if (string.IsNullOrEmpty(imagePath)
+                || !cropPointX.HasValue
+                || !cropPointY.HasValue
+                || !imageCropWidth.HasValue
+                || !imageCropHeight.HasValue)
+            {
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+
+            byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
+            byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
+
+            string tempFolderName = Server.MapPath("~/" + ConfigurationManager.AppSettings["Image.TempFolderName"]);
+            string fileName = Path.GetFileName(imagePath);
+
+            try
+            {
+                FileHelper.SaveFile(croppedImage, Path.Combine(tempFolderName, fileName));
+            }
+            catch (Exception ex)
+            {
+                //Log an error     
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
+
+            string photoPath = string.Concat("/", ConfigurationManager.AppSettings["Image.TempFolderName"], "/", fileName);
+            return Json(new { photoPath = photoPath }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
